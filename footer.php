a0fb548b146ddfeb0bbd0953acb963e494a7f943
Sam Robets <samroberts707@gmail.com>
2022-01-26 22:28:35 +0000

    Finish footer

--- footer.php
+++ footer.php
@@ -19,6 +19,19 @@
         </div>
         <div class="get-in-touch-block">
           <p><strong>Get in touch</strong></p>
+          <div class="contact-details">
+            <p><strong>T</strong> 0330 223 6224</p>
+            <p><strong>E</strong> hello@thefutureneers.com</p>
+          </div>
+          <div class="address">
+            <p>The Futureneers,</p>
+            <p>Kemp House 152-160 City Road,</p>
+            <p>London, England, EC1V 2NX</p>
+          </div>
+        </div>
+        <div class="legal-block">
+          <p class="small-text">&copy; <?php echo date('Y'); ?> The Futureneers Social Enterprise CIC Company number: 13689026 – Registered in England</p>
+          <p class="small-text"><a href="/privacy-policy/">Privacy policy</a></p>
         </div>
       </div>
     </footer>
