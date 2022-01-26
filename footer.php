7891ed21140fa428d393a2aef7063aaf81d495d5
Sam Robets <samroberts707@gmail.com>
2022-01-26 23:00:43 +0000

    Add rel attrs to social links

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
     <footer>
       <div class="container">
         <div class="socials">
-          <a href="https://www.facebook.com/TheFutureneers/" target="_blank" class="social-link">
+          <a href="https://www.facebook.com/TheFutureneers/" target="_blank" rel="me" class="social-link">
             <img class="social-logo" alt="Facebook" src="<?php echo get_template_directory_uri().'/src/images/icons/facebook.svg'; ?>" />
           </a>
-          <a href="https://google.com" target="_blank" class="social-link">
+          <a href="https://google.com" target="_blank" rel="me" class="social-link">
             <img class="social-logo" alt="TikTok" src="<?php echo get_template_directory_uri().'/src/images/icons/tiktok.svg'; ?>" />
           </a>
-          <a href="https://google.com" target="_blank" class="social-link">
+          <a href="https://google.com" target="_blank" rel="me" class="social-link">
             <img class="social-logo" alt="Twitter" src="<?php echo get_template_directory_uri().'/src/images/icons/twitter.svg'; ?>" />
           </a>
-          <a href="https://google.com" target="_blank" class="social-link">
+          <a href="https://google.com" target="_blank" rel="me" class="social-link">
             <img class="social-logo" alt="LinkedIn" src="<?php echo get_template_directory_uri().'/src/images/icons/linkedin.svg'; ?>" />
           </a>
-          <a href="https://google.com" target="_blank" class="social-link">
+          <a href="https://google.com" target="_blank" rel="me" class="social-link">
             <img class="social-logo" alt="Instagram" src="<?php echo get_template_directory_uri().'/src/images/icons/instagram.svg'; ?>" />
           </a>
         </div>
