e16a02747e66259be62c18781fef5f6031a205e8
Sam Robets <samroberts707@gmail.com>
2022-01-26 21:57:16 +0000

    Add typography

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
-
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
+<blockquote>Quote</blockquote>
+<p>Paragraph</p>
 <?php get_footer(); ?>
