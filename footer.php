1048f923af632c076e865e814d2aa684cd85cd7c
Sam Robets <samroberts707@gmail.com>
2022-01-26 21:41:30 +0000

    Add socials to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,26 @@
     <footer>
-      
+      <div class="container">
+        <div class="socials">
+          <a href="https://www.facebook.com/TheFutureneers/" target="_blank" class="social-link">
+            <img class="social-logo" alt="Facebook" src="<?php echo get_template_directory_uri().'/src/images/icons/facebook.svg'; ?>" />
+          </a>
+          <a href="https://google.com" target="_blank" class="social-link">
+            <img class="social-logo" alt="TikTok" src="<?php echo get_template_directory_uri().'/src/images/icons/tiktok.svg'; ?>" />
+          </a>
+          <a href="https://google.com" target="_blank" class="social-link">
+            <img class="social-logo" alt="Twitter" src="<?php echo get_template_directory_uri().'/src/images/icons/twitter.svg'; ?>" />
+          </a>
+          <a href="https://google.com" target="_blank" class="social-link">
+            <img class="social-logo" alt="LinkedIn" src="<?php echo get_template_directory_uri().'/src/images/icons/linkedin.svg'; ?>" />
+          </a>
+          <a href="https://google.com" target="_blank" class="social-link">
+            <img class="social-logo" alt="Instagram" src="<?php echo get_template_directory_uri().'/src/images/icons/instagram.svg'; ?>" />
+          </a>
+        </div>
+        <div class="get-in-touch-block">
+          <p><strong>Get in touch</strong></p>
+        </div>
+      </div>
     </footer>
     <?php wp_footer(); ?>
   </body>
